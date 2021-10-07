ad14e6a077b37356166c62b867d1e3debe09d267
Felipe Pilz <felipevilvertpilz@gmail.com>
2021-10-07 17:16:14 -0300

    Random choice to enemy player added

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-var elements = document.querySelectorAll('.player-options div > img');
+var playerElements = document.querySelectorAll('.player-options div > img');
+var enemyElements = document.querySelectorAll('.enemy-options div > img');
 var playerOpt = "";
+var enemyOpt = "";
 
-const resetOpacityPlayer = ()=>{
-    for(var i=0; i < elements.length; i++){
-            elements[i].style.opacity = 0.3;
+const enemyChoice = ()=>{
+    let rand = Math.floor(Math.random()*3);
+    for(var i = 0; i < enemyElements.length; i++){
+        if(i == rand){
+            resetOpacityEnemy();
+            enemyElements[i].style.opacity = 1;
+            enemyOpt = enemyElements[i].getAttribute('opt');
+        }
     }
 }
 
-for(var i=0; i < elements.length; i++){
-    elements[i].addEventListener('click',(t)=>{
+const resetOpacityEnemy = ()=>{
+    for(var i=0; i < enemyElements.length; i++){
+        enemyElements[i].style.opacity = 0.3;
+    }
+}
+
+
+const resetOpacityPlayer = ()=>{
+    for(var i=0; i < playerElements.length; i++){
+        playerElements[i].style.opacity = 0.3;
+    }
+}
+
+for(var i=0; i < playerElements.length; i++){
+    playerElements[i].addEventListener('click',(t)=>{
         resetOpacityPlayer();
         t.target.style.opacity = 1;
         playerOpt = t.target.getAttribute("opt");
+
+        enemyChoice();
     })
 }
